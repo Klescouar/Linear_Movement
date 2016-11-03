db47a49ff197321065c360af3d98e026e6322da6
J2A2B <ja.andreani@gmail.com>
2016-11-03 10:32:21 +0100

    pull kev

--- public/assets/js/controllers/homeCtrl.js
+++ public/assets/js/controllers/homeCtrl.js
@@ -3,7 +3,6 @@ $scope.changeEvent = true;
 
 $scope.changeZIndex = function(selector) {
 
-console.log(selector);
 var index_highest = 0;
 // more effective to have a class for the div you want to search
 $(".img-middle,.video-middle,.info-middle").each(function() {
@@ -30,12 +29,65 @@ if (selector === 3) {
 $(document).ready(function() {
    
 $( '.draggable' )
-    	.draggable({
-    	});
+    	.draggable();
 $('#resize')
     	.resizable();
+
+var iframe = $('.iframe-home');
+var player = new Vimeo.Player(iframe);
+
+$scope.playVideo = function() {
+
+	$('.player .controls').css('display', 'none!important');
+      //display: none!important;
+
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    console.log("player.getPaused()", player.getPaused());
+
+	player.getPaused()
+		.then(function(paused){
+			playPause(paused);			
+		});
+
+	function playPause(paused){
+
+		if(paused){
+			player.play();
+
+		} else{
+		    	console.log('pause it!');
+		    	player.pause();
+		 }
+
+	}		
+
+	// 	player.play();
+ //     		console.log('play it!');
+
+
+ //    if(player.getPaused().then(function(){
+	// player.play();
+ //     	console.log('play it!');
+ //    } ) )
     
 
+         	
+    // } else{
+    // 	console.log('pause it!');
+    // 	player.pause();
+    // }
+   
+}
+
 });
+
+
+
+
+
 
 }]);﻿
